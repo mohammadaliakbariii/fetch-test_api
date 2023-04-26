eb33e748cf3ff7816491323f62a7506c0709ec66
mohammad <aliakbariiimohammad@gmail.com>
2023-04-26 15:14:11 +0430

    fetch user of posts and complete the blog

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-let url = 'https://jsonplaceholder.typicode.com/users'
-let app = document.getElementById('id')
+let post_url = 'https://jsonplaceholder.typicode.com/posts/'
+let user_urls = 'https://jsonplaceholder.typicode.com/users/'
+let app = document.querySelector('#app')
 
+async function getAuthor(url,id){
+    let response = await fetch(url+id)
+    let author = await response.json()
+    return author
+}
 // fetch(url)
 // .then(response =>response.json())
 // .then(data => console.log(data))
@@ -8,7 +14,17 @@ let app = document.getElementById('id')
 async function getArticles(url){
     let response = await fetch(url)
     let data = await response.json()
-    // console.log(data)
+    console.log(data)
+    data.forEach(async post => {
+        post.user = await getAuthor(user_urls,post.userId)
+        app.innerHTML += 
+        `<article>
+        <h2 class = 'h4'>${post.title}</h2>
+        <small class = 'badge badge-danger'>${post.user.name}</small>
+        <p>${post.body}</p>
+        </article>
+        <hr>`
+    });
 }
 
-getArticles(url)
+getArticles(post_url)
